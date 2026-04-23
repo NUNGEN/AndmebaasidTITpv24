a7c60d1780acfcdb456914ad664f4587bb714ecc
NUNGEN <nikita.voronin0211@gmail.com>
2026-04-23 11:28:16 +0300

    värskendunud andmebaas

--- CREATE_INSERT_NV.sql
+++ CREATE_INSERT_NV.sql
@@ -1,26 +1,34 @@
 CREATE DATABASE Opetaja;
 
-use Voronin_baas
---tabeli loomine
-CREATE TABLE opetaja(
-opetajaID int Primary Key (1),
-nimi varchar(25),
-epost TEXT,
-ruum varchar(25) );
+USE Opetaja;
+
+CREATE TABLE opetaja (
+    opetajaID INT PRIMARY KEY,
+    nimi VARCHAR(25),
+    epost TEXT,
+    ruum VARCHAR(25)
+);
 
 SELECT * FROM opetaja;
--- lisame nimi, epost ja ruum tabelisse
-INSERT INTO opetaja,
-VALUES ('1','Markus','markus@.com';
 
-INSERT INTO opetaja(opetajaID, nimi, epost, ruum)
-VALUES ('19374','MoriartiMorio','Morio@gmail.com'),
-('09384','JohannesJaanes','Jaanes@gmail.com'),
-('56012','KuldnarKuuli','Kuuli@gmail.com' );
+INSERT INTO opetaja (opetajaID, nimi, epost, ruum)
+VALUES (1, 'Markus', 'markus@.com', '101');
 
+INSERT INTO opetaja (opetajaID, nimi, epost, ruum)
+VALUES 
+(19374, 'MoriartiMorio', 'Morio@gmail.com', '33'),
+(9384, 'JohannesJaanes', 'Jaanes@gmail.com', '33'),
+(56012, 'KuldnarKuuli', 'Kuuli@gmail.com', '33');
 
-UPDATE opetaja SET strip=1, aadress='Tallinn';
+ALTER TABLE opetaja
+ADD strip INT,
+ADD aadress VARCHAR(50);
 
-UPDATE opetaja SET strip=1, aadress='Tallinn' WHERE opetajaID=5;
+UPDATE opetaja 
+SET strip = 1, aadress = 'Tallinn';
+
+UPDATE opetaja 
+SET strip = 1, aadress = 'Tallinn' 
+WHERE opetajaID = 5;
 
 SELECT * FROM opetaja;
